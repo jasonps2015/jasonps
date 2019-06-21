c98b96830c281611d8cd4d2559f8b3770002ee36
jasonps2019 <jasonps2019@gmail.com>
2019-06-21 08:47:24 +0800

    component   组件

---
+++ controllers/layuiadmingo_component.go
@@ -0,0 +1,618 @@
+package controllers
+
+
+func (c *LayuiadminGOController) ComponentAnimIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/anim/index.html"
+}
+
+func (c *LayuiadminGOController) ComponentAuxiliarIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/auxiliar/index.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentBadgeIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/badge/index.html"
+}
+
+func (c *LayuiadminGOController) ComponentButtonIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/button/index.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentCarouselIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/carousel/index.html"
+}
+
+func (c *LayuiadminGOController) ComponentCodeIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/code/index.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentColorpickerIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/colorpicker/index.html"
+}
+
+func (c *LayuiadminGOController) ComponentFlowIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/flow/index.html"
+}
+
+
+//component form
+func (c *LayuiadminGOController) ComponentFormElement() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/form/element.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentFormGroup() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/form/group.html"
+}
+
+
+//component grid
+func (c *LayuiadminGOController) ComponentGridAll() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/grid/all.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentGridList() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/grid/list.html"
+}
+
+func (c *LayuiadminGOController) ComponentGridMobile() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/grid/mobile.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentGridMobile_pc() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/grid/mobile-pc.html"
+}
+
+func (c *LayuiadminGOController) ComponentGridSpeed_dial() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/grid/speed-dial.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentGridStack() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/grid/stack.html"
+}
+
+
+
+//component laydate
+func (c *LayuiadminGOController) ComponentLaydateIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/laydate/index.html"
+}
+
+func (c *LayuiadminGOController) ComponentLaydateDemo1() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/laydate/demo1.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentLaydateDemo2() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/laydate/demo2.html"
+}
+
+func (c *LayuiadminGOController) ComponentLaydateSpecial_demo() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/laydate/special-demo.html"
+}
+
+func (c *LayuiadminGOController) ComponentLaydateTheme() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/laydate/theme.html"
+}
+
+
+
+//component layer
+func (c *LayuiadminGOController) ComponentLayerList() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/layer/list.html"
+}
+
+func (c *LayuiadminGOController) ComponentLayerSpecial_demo() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/layer/special-demo.html"
+}
+
+func (c *LayuiadminGOController) ComponentLayerTheme() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/layer/theme.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentLaypageIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/laypage/index.html"
+}
+
+func (c *LayuiadminGOController) ComponentLaypageDemo1() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/laypage/demo1.html"
+}
+
+func (c *LayuiadminGOController) ComponentLaypageDemo2() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/laypage/demo2.html"
+}
+
+
+
+
+func (c *LayuiadminGOController) ComponentLaytplIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["d_list_length"] = false
+	c.Data["d_title"] = "astaxie"
+
+	c.TplName = "layuiadmingo/component/laytpl/index.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentNavIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/nav/index.html"
+}
+
+func (c *LayuiadminGOController) ComponentPanelIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/panel/index.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentProgressIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/progress/index.html"
+}
+
+
+
+func (c *LayuiadminGOController) ComponentRateIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/rate/index.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentSliderIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/slider/index.html"
+}
+
+
+//component table
+func (c *LayuiadminGOController) ComponentTableAuto() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/auto.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentTableCellEdit() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/cellEdit.html"
+}
+
+func (c *LayuiadminGOController) ComponentTableCellEvent() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/cellEvent.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentTableCheckbox() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/checkbox.html"
+}
+
+func (c *LayuiadminGOController) ComponentTableData() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/data.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentTableFixed() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/fixed.html"
+}
+
+func (c *LayuiadminGOController) ComponentTableForm() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["d_id"] = "1003"
+
+	c.TplName = "layuiadmingo/component/table/form.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentTableHeight() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/height.html"
+}
+
+func (c *LayuiadminGOController) ComponentTableIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "/component/table/index.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentTableInitSort() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/initSort.html"
+}
+
+func (c *LayuiadminGOController) ComponentTableOnrow() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/onrow.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentTableOperate() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/operate.html"
+}
+
+func (c *LayuiadminGOController) ComponentTablePage() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/page.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentTableParseData() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/parseData.html"
+}
+
+func (c *LayuiadminGOController) ComponentTableRadio() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/radio.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentTableReload() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/reload.html"
+}
+
+func (c *LayuiadminGOController) ComponentTableResetPage() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/resetPage.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentTableSimple() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/simple.html"
+}
+
+func (c *LayuiadminGOController) ComponentTableStatic() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/static.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentTableStyle() {
+	c.Data["d_sex"] = true  //女
+	c.Data["d_username"] = "astaxie@gmail.com"
+	c.Data["d_id"] = "35"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/style.html"
+}
+
+func (c *LayuiadminGOController) ComponentTableThead() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/thead.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentTableToolbar() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/toolbar.html"
+}
+
+func (c *LayuiadminGOController) ComponentTableTostatic() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/tostatic.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentTableTotalRow() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/table/totalRow.html"
+}
+
+func (c *LayuiadminGOController) ComponentTabsIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/tabs/index.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentTimelineIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/timeline/index.html"
+}
+
+
+
+//component upload
+func (c *LayuiadminGOController) ComponentUploadIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/upload/index.html"
+}
+
+func (c *LayuiadminGOController) ComponentUploadDemo1() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/upload/demo1.html"
+}
+
+
+func (c *LayuiadminGOController) ComponentUploadDemo2() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/upload/demo2.html"
+}
+
+func (c *LayuiadminGOController) ComponentUtilIndex() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["layui_admin_v"] = "layuiAdmin-v1.2.1 std"
+	c.Data["layui_v"] = "2.4.5"
+
+	c.TplName = "layuiadmingo/component/util/index.html"
+}
+
+
+
--- routers/router.go
+++ routers/router.go
@@ -92,10 +92,123 @@ func init() {
 	beego.Router("/Iframe/Layer/Iframe", &layuiadmingo.LayuiadminGOController{},"*:IframeLayerIframe")
 
 
+	//system
 	beego.Router("/system", &layuiadmingo.LayuiadminGOController{},"*:SystemAbout")
 	beego.Router("/system/about", &layuiadmingo.LayuiadminGOController{},"*:SystemAbout")
 	beego.Router("/system/get", &layuiadmingo.LayuiadminGOController{},"*:SystemGet")
 	beego.Router("/system/more", &layuiadmingo.LayuiadminGOController{},"*:SystemMore")
 	beego.Router("/system/theme", &layuiadmingo.LayuiadminGOController{},"*:SystemTheme")
+
+	//component
+	beego.Router("/component/", &layuiadmingo.LayuiadminGOController{},"*:ComponentAnimIndex")
+	beego.Router("/component/anim", &layuiadmingo.LayuiadminGOController{},"*:ComponentAnimIndex")
+	beego.Router("/component/auxiliar", &layuiadmingo.LayuiadminGOController{},"*:ComponentAuxiliarIndex")
+	beego.Router("/component/badge", &layuiadmingo.LayuiadminGOController{},"*:ComponentBadgeIndex")
+	beego.Router("/component/button", &layuiadmingo.LayuiadminGOController{},"*:ComponentButtonIndex")
+	beego.Router("/component/carousel", &layuiadmingo.LayuiadminGOController{},"*:ComponentCarouselIndex")
+	beego.Router("/component/code", &layuiadmingo.LayuiadminGOController{},"*:ComponentCodeIndex")
+	beego.Router("/component/colorpicker", &layuiadmingo.LayuiadminGOController{},"*:ComponentColorpickerIndex")
+	beego.Router("/component/flow", &layuiadmingo.LayuiadminGOController{},"*:ComponentFlowIndex")
+
+	beego.Router("/component/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentAnimIndex")
+	beego.Router("/component/anim/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentAnimIndex")
+	beego.Router("/component/auxiliar/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentAuxiliarIndex")
+	beego.Router("/component/badge/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentBadgeIndex")
+	beego.Router("/component/button/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentButtonIndex")
+	beego.Router("/component/carousel/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentCarouselIndex")
+	beego.Router("/component/code/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentCodeIndex")
+	beego.Router("/component/colorpicker/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentColorpickerIndex")
+	beego.Router("/component/flow/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentFlowIndex")
+
+	//component form
+	beego.Router("/component/form", &layuiadmingo.LayuiadminGOController{},"*:ComponentFormElement")
+	beego.Router("/component/form/element", &layuiadmingo.LayuiadminGOController{},"*:ComponentFormElement")
+	beego.Router("/component/form/group", &layuiadmingo.LayuiadminGOController{},"*:ComponentFormGroup")
+	//component grid
+	beego.Router("/component/grid", &layuiadmingo.LayuiadminGOController{},"*:ComponentGridAll")
+	beego.Router("/component/grid/all", &layuiadmingo.LayuiadminGOController{},"*:ComponentGridAll")
+	beego.Router("/component/grid/list", &layuiadmingo.LayuiadminGOController{},"*:ComponentGridList")
+	beego.Router("/component/grid/mobile", &layuiadmingo.LayuiadminGOController{},"*:ComponentGridMobile")
+	beego.Router("/component/grid/mobile-pc", &layuiadmingo.LayuiadminGOController{},"*:ComponentGridMobile_pc")
+	beego.Router("/component/grid/speed-dial", &layuiadmingo.LayuiadminGOController{},"*:ComponentGridSpeed_dial")
+	beego.Router("/component/grid/stack", &layuiadmingo.LayuiadminGOController{},"*:ComponentGridStack")
+
+
+
+	//component laydate
+	beego.Router("/component/laydate", &layuiadmingo.LayuiadminGOController{},"*:ComponentLaydateIndex")
+	beego.Router("/component/laydate/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentLaydateIndex")
+	beego.Router("/component/laydate/demo1", &layuiadmingo.LayuiadminGOController{},"*:ComponentLaydateDemo1")
+	beego.Router("/component/laydate/demo2", &layuiadmingo.LayuiadminGOController{},"*:ComponentLaydateDemo2")
+	beego.Router("/component/laydate/special-demo", &layuiadmingo.LayuiadminGOController{},"*:ComponentLaydateSpecial_demo")
+	beego.Router("/component/laydate/theme", &layuiadmingo.LayuiadminGOController{},"*:ComponentLaydateTheme")
+
+	//beego.Router("/component/layer", &layuiadmingo.LayuiadminGOController{},"*:ComponentLayerList")
+	beego.Router("/component/layer/list", &layuiadmingo.LayuiadminGOController{},"*:ComponentLayerList")
+	beego.Router("/component/layer/special-demo", &layuiadmingo.LayuiadminGOController{},"*:ComponentLayerSpecial_demo")
+	beego.Router("/component/layer/theme", &layuiadmingo.LayuiadminGOController{},"*:ComponentLayerTheme")
+
+	beego.Router("/component/laypage", &layuiadmingo.LayuiadminGOController{},"*:ComponentLaypageIndex")
+	beego.Router("/component/laytpl", &layuiadmingo.LayuiadminGOController{},"*:ComponentLaytplIndex")
+	beego.Router("/component/nav/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentNavIndex")
+	beego.Router("/component/panel", &layuiadmingo.LayuiadminGOController{},"*:ComponentPanelIndex")
+	beego.Router("/component/progress", &layuiadmingo.LayuiadminGOController{},"*:ComponentProgressIndex")
+	beego.Router("/component/rate", &layuiadmingo.LayuiadminGOController{},"*:ComponentRateIndex")
+	beego.Router("/component/slider", &layuiadmingo.LayuiadminGOController{},"*:ComponentSliderIndex")
+
+
+	///component laypage
+	beego.Router("/component/laypage", &layuiadmingo.LayuiadminGOController{},"*:ComponentLaypageIndex")
+	beego.Router("/component/laypage/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentLaypageIndex")
+	beego.Router("/component/laypage/demo1", &layuiadmingo.LayuiadminGOController{},"*:ComponentLaypageDemo1")
+	beego.Router("/component/laypage/demo2", &layuiadmingo.LayuiadminGOController{},"*:ComponentLaypageDemo2")
+
+	beego.Router("/component/laytpl/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentLaytplIndex")
+	beego.Router("/component/nav/index/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentNavIndex")
+	beego.Router("/component/panel/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentPanelIndex")
+	beego.Router("/component/progress/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentProgressIndex")
+	beego.Router("/component/rate/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentRateIndex")
+	beego.Router("/component/slider/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentSliderIndex")
+
+	//component table
+	beego.Router("/component/table/auto", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableAuto")
+	beego.Router("/component/table/cellEdit", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableCellEdit")
+	beego.Router("/component/table/cellEvent", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableCellEvent")
+	beego.Router("/component/table/checkbox", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableCheckbox")
+	beego.Router("/component/table/data", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableData")
+	beego.Router("/component/table/fixed", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableFixed")
+	beego.Router("/component/table/form", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableForm")
+	beego.Router("/component/table/height", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableHeight")
+	beego.Router("/component/table/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableIndex")
+	beego.Router("/component/table/initSort", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableInitSort")
+	beego.Router("/component/table/onrow", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableOnrow")
+	beego.Router("/component/table/operate", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableOperate")
+	beego.Router("/component/table/page", &layuiadmingo.LayuiadminGOController{},"*:ComponentTablePage")
+	beego.Router("/component/table/parseData", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableParseData")
+	beego.Router("/component/table/radio", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableRadio")
+	beego.Router("/component/table/reload", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableReload")
+	beego.Router("/component/table/resetPage", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableResetPage")
+	beego.Router("/component/table/simple", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableSimple")
+	beego.Router("/component/table/static", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableStatic")
+	beego.Router("/component/table/style", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableStyle")
+	beego.Router("/component/table/thead", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableThead")
+	beego.Router("/component/table/toolbar", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableToolbar")
+	beego.Router("/component/table/tostatic", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableTostatic")
+	beego.Router("/component/table/totalRow", &layuiadmingo.LayuiadminGOController{},"*:ComponentTableTotalRow")
+
+
+	beego.Router("/component/tabs", &layuiadmingo.LayuiadminGOController{},"*:ComponentTabsIndex")
+	beego.Router("/component/timeline", &layuiadmingo.LayuiadminGOController{},"*:ComponentTimelineIndex")
+
+	beego.Router("/component/tabs/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentTabsIndex")
+	beego.Router("/component/timeline/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentTimelineIndex")
+
+	//component upload
+	beego.Router("/component/upload", &layuiadmingo.LayuiadminGOController{},"*:ComponentUploadIndex")
+	beego.Router("/component/upload/demo1", &layuiadmingo.LayuiadminGOController{},"*:ComponentUploadDemo1")
+	beego.Router("/component/upload/demo2", &layuiadmingo.LayuiadminGOController{},"*:ComponentUploadDemo2")
+
+	beego.Router("/component/util", &layuiadmingo.LayuiadminGOController{},"*:ComponentUtilIndex")
+	beego.Router("/component/util/index", &layuiadmingo.LayuiadminGOController{},"*:ComponentUtilIndex")
 
 }
